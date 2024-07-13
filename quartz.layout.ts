57cb9d22088b9190e5e49c7aa4360d3cbb7ecdda
“rustam-aka-rust” <rustam.agamaliev@gmail.com>
2024-07-13 21:49:16 +0300

    vault backup: 2024-07-13 21:49:16

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      Рустам Агамалиев: "https://rustamagamaliev.ru",
-      "Telegram": "https://t.me/zettelkasten_ch",
+      GitHub: "https://github.com/jackyzha0/quartz",
+      "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
