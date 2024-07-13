f4644f5184db9d145cbc5eb686d72d0f71e3a520
“rustam-aka-rust” <rustam.agamaliev@gmail.com>
2024-07-13 21:11:05 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🌳Цифровой садик",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
